2377338f2ffb942ac4b35efed84d56f32e4ac906
utente <utente@LAPTOP-GGB45N4B>
2019-10-11 21:09:46 +0200

    AlienDictionary completato

--- Lab02_Alien/src/it/polito/tdp/alien/AlienDictionary.java
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienDictionary.java
@@ -11,11 +11,41 @@ public class AlienDictionary {
 
 
      
-	public void aggiungiTraduzione(WordEnhanced aW) {
-		if (!this.listaW.contains(aW))
-		this.listaW.add(aW);
+	public void aggiungiTraduzione(String aW, String trad) {
+		WordEnhanced word = new WordEnhanced (aW);
+		if (this.listaW.contains(word)) {
+			listaW.get(listaW.indexOf(word)).setTraduzioni(trad);
+		}else {
+			listaW.add(word);
+			word.setTraduzioni(trad);
+		}
+		
 	}
 	public List<WordEnhanced> getListaW() {
 		return listaW;
+	}
+	public String cercaTraduzioniWild (String aW) {
+		for (WordEnhanced w : listaW) {
+			if (w.getaW().equals(aW)) {
+				w.getTraduzioni();
+			}
+		}
+		return "";
+	}
+	public String cercaParolaConWildCard (String wild) {
+		wild = wild.replace("\\?", ".");
+		boolean trovati =false;
+		StringBuilder sb = new StringBuilder();
+		for (WordEnhanced w : listaW) {
+			if (w.compareWild(wild)){
+				trovati = true;
+				sb.append(w.getTraduzioni()+"\n");
+			}
+		}
+		if (trovati==true) {
+			 return sb.toString();
+			
+		}else
+			return null;
 	}
 	}
